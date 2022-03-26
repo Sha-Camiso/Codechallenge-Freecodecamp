3f07efd74940a4c38f38764f47a5db7577b91389
Sha <99729188+Sha-Camiso@users.noreply.github.com>
2022-03-26 20:50:59 +0900

    Update Challenges.js

--- Challenges.js
+++ Challenges.js
@@ -121,3 +121,232 @@ function processArg(num) {
 
 // Only change code below this line
 processed = processArg(7);
+
+function testSize(num) {
+  // Only change code below this line
+if (num<5){
+return "Tiny";
+} else if (num<10){
+  return "Small";
+} else if (num<15){
+  return "Medium";
+} else if (num<20){
+  return "Large";
+} else if (num>=20){
+  return "Huge";
+}
+  
+  // Only change code above this line
+}
+
+testSize(7);
+
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+if (strokes==1){
+return names[0];
+} else if (strokes<=par-2){
+  return names[1];
+} else if (strokes===par-1) {
+  return names[2];
+} else if (strokes===par){
+  return names[3]
+} else if (strokes===par+1){
+  return names[4];
+} else if (strokes===par+2){
+  return names[5];
+} else  (strokes >= par+3); {
+  return names[6];}
+  // Only change code above this line
+}
+
+golfScore(5, 4);
+
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+
+  switch (val) {
+    case 1: answer ="alpha"; break;
+    case 2: answer = "beta"; break;
+    case 3: answer = "gamma"; break;
+    case 4: answer ="delta"; break;
+  }
+
+
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
+
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+switch(val){
+  case "a": answer="apple";break;
+  case "b": answer="bird";break;
+  case "c": answer="cat";break;
+  default: answer="stuff";break;
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
+
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+ switch (val){
+   case 1:
+   case 2:
+   case 3:
+    answer="Low";
+    break;
+     case 4:
+   case 5:
+   case 6:
+    answer="Mid";
+    break;
+     case 7:
+   case 8:
+   case 9:
+    answer="High";
+    break;
+ }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
+
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch (val){
+    case "bob": answer="Marley";break
+    case 42: answer="The Answer";break
+    case 1: answer="There is no #1";break
+    case 99: answer="Missed me by this much!";break
+    case 7: answer="Ate Nine";break
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
+
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+  
+  // Only change code above this line
+}
+
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+function checkObj(obj, checkProp) {
+  // Only change code below this line
+  if (obj.hasOwnProperty(checkProp) ){
+
+  
+ return obj[checkProp];
+} else{
+  return "Not Found";
+  // Only change code above this line
+}
+}
+
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true,
+  },{
+   "artist": "Celine Dion",
+    "title": "Piano Man",
+    "release_year": 1993,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
+
+// Setup
+var recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if (prop !== 'tracks' && value !== "") {
+    records[id][prop] = value;
+  } else if (prop === "tracks" && records[id].hasOwnProperty("tracks") === false) {
+    records[id][prop] = [value];
+  } else if (prop === "tracks" && value !== "") {
+    records[id][prop].push(value);
+  } else if (value === "") {
+    delete records[id][prop];
+  }
+  return records;
+}
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+
+
