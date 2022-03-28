c825a9b437c20a8b3ead143679c86ff12e5e3b1a
Sha <99729188+Sha-Camiso@users.noreply.github.com>
2022-03-28 11:49:22 +0900

    Update Challenges.js

--- Challenges.js
+++ Challenges.js
@@ -357,3 +357,38 @@ function sum(arr, n) {
     return sum(arr, n - 1) + arr[n - 1];
   }
 }
+
+function convertToInteger(str) {
+return parseInt(str);
+}
+
+convertToInteger("56");
+
+  return num > 0 ? "positive"
+    : num < 0 ? "negative"
+    : "zero";
+}
+
+// Only change code below this line
+function countdown(n){
+    if (n < 1) {
+    return [];
+  } else {
+    const arr = countdown(n - 1);
+    arr.unshift(n);
+    return arr;
+  }
+
+}
+// Only change code above this line
+
+function rangeOfNumbers(startNum, endNum) {
+  
+  if (endNum - startNum === 0) {
+    return [startNum];
+  } else {
+    var numbers = rangeOfNumbers(startNum, endNum - 1);
+    numbers.push(endNum);
+    return numbers;
+  }
+}
